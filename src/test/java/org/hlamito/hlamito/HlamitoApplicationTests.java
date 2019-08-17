b54979d43e846b38d9b112c32bbd7fdbba69f794
alls23 <alls232323@gmail.com>
2019-08-17 13:45:43 +0200

    project creating

---
+++ src/test/java/org/hlamito/hlamito/HlamitoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.hlamito.hlamito;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HlamitoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
