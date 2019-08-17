05d9a456463e094f37f72d35b3d67e9cc1773316
alls23 <alls232323@gmail.com>
2019-08-17 14:31:29 +0200

    project creating

--- src/main/java/org/hlamito/hlamito/HlamitoApplication.java
+++ src/main/java/org/hlamito/hlamito/HlamitoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HlamitoApplication {
-//	sdsds
+//	sguigtiugdsds
 
 	public static void main(String[] args) {
 		SpringApplication.run(HlamitoApplication.class, args);
