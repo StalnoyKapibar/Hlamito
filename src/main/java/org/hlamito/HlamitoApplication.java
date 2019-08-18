0f2ec43340526a52319311f1a358df9d5879d0c6
ZuiQuanZ <123casper@mail.ru>
2019-08-18 17:17:34 +0300

    добавлен метод поиска по названию поля

---
+++ src/main/java/org/hlamito/HlamitoApplication.java
@@ -0,0 +1,13 @@
+package org.hlamito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlamitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlamitoApplication.class, args);
+	}
+
+}
